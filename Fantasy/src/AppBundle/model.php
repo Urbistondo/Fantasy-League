5293174b5b0e0a0eecb964b582ff59d855d7d8d4
Javi Urbistondo <Javi Urbistondo>
2015-12-15 18:50:35 +0100

    Problemas iniciales arreglados

--- Fantasy/src/AppBundle/model.php
+++ Fantasy/src/AppBundle/model.php
@@ -1,7 +1,7 @@
 <?php
 function open_database_connection()
 {
-	$link = mysql_connect('localhost', 'root', 'root');
+	$link = mysql_connect('localhost', 'root', 'fantasy');
 	mysql_select_db('fantasy_league_db', $link);
 
 	return $link;
@@ -16,7 +16,7 @@ function get_all_posts()
 {
 	$link = open_database_connection();
 
-	$result = mysql_query('SELECT id, name, lastname, club, position, birthDate, nationality FROM jugador', $link);
+	$result = mysql_query('SELECT id, name, lastname, birthDate, nationality, club, position FROM player', $link);
 	$players = array();
 	while ($row = mysql_fetch_assoc($result)) {
 		$players[] = $row;
@@ -32,7 +32,7 @@ function get_post_by_id($id)
 	$link = open_database_connection();
 
 	$id = intval($id);
-	$query = 'SELECT id, name, lastname, club, position, birthDate, nationality FROM jugador WHERE id = '.$id;
+	$query = 'SELECT id, name, lastname, birthDate, nationality, club, position FROM player WHERE id = '.$id;
 	$result = mysql_query($query);
 	$row = mysql_fetch_assoc($result);
 
