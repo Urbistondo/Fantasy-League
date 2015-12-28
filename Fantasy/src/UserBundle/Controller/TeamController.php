ab3f3e729a00590e6f6182daa6a7568de19395bc
Javi Urbistondo <Javi Urbistondo>
2015-12-28 21:42:58 +0100

    Alineacion

--- Fantasy/src/UserBundle/Controller/TeamController.php
+++ Fantasy/src/UserBundle/Controller/TeamController.php
@@ -27,22 +27,38 @@ class TeamController extends Controller
 	{
 		return $this->render('UserBundle:User:teamform.html.twig');
 	}
-	public function showTeamAction($league_id, $team_id)
+	public function showTeamAction($league_id, $team_id, $modify)
 	{
 		$session=$this->getRequest()->getSession();
 		$session->set('league_id', $league_id);
-		$user=$session->get('user');
-		$user_id = $user->getId();
-		$eleven = $this->get('doctrine')->getManager()->getRepository('UserBundle:Eleven')->findOneBy(array('user_id' => $user_id));
-		$aux = array();
-		$aux = $eleven->getPlayers();
-		$players = array();
-		foreach ($aux as $id)
+		$session->set('team_id', $team_id);
+		if ($modify == "false")
 		{
-			$player = $this->get('doctrine')->getManager()->getRepository('UserBundle:Player')->findOneBy(array('id' => $id));
-			array_push($players, $player);
+			$eleven = $this->get('doctrine')->getManager()->getRepository('UserBundle:Eleven')->findOneBy(array('team_id' => $team_id));
+			$aux = array();
+			$aux = $eleven->getPlayers();
+			$players = array();
+			foreach ($aux as $id)
+			{
+				$player = $this->get('doctrine')->getManager()->getRepository('UserBundle:Player')->findOneBy(array('id' => $id));
+				array_push($players, $player);
+			}
+			return $this->render('UserBundle:User:list.html.twig', array('items' => $players, 'title' => "Starting eleven", 'message' => false, 
+				'type' => "Player", 'modify' => $modify));
 		}
-		return $this->render('UserBundle:User:list.html.twig', array('items' => $players, 'title' => "Starting eleven", 'message' => false, 'type' => "Player", 'team' => $team_id));
+		else
+		{
+			$player_ids = $this->get('doctrine')->getManager()->getRepository('UserBundle:Belong')->findBy(array('team_id' => $team_id));
+			$players = array();
+			foreach ($player_ids as $id)
+			{
+				$player = $this->get('doctrine')->getManager()->getRepository('UserBundle:Player')->findOneBy(array('id' => $id));
+				array_push($players, $player);
+			}
+			return $this->render('UserBundle:User:list.html.twig', array('items' => $players, 'title' => "Choose your starting eleven", 'message' => false, 
+				'type' => "Player", 'modify' => $modify));
+		}
+		
 	}
 
 	public function createTeamAction()
@@ -56,6 +72,53 @@ class TeamController extends Controller
 
 	public function modifyElevenAction()
 	{
+		$session = $this->getRequest()->getSession();
+		$league_id = $session->get('league_id');
+		$team_id = $session->get('team_id');
+		return $this->redirectToRoute('user_showTeam', array('league_id' => $league_id, 'team_id' => $team_id, 'modify' => "true"), 301);
+	}
+
+	public function updateElevenAction(Request $request)
+	{
+		$session = $this->getRequest()->getSession();
+		$league_id = $session->get('league_id');
+		$team_id = $session->get('team_id');
+
+		$em = $this->getDoctrine()->getEntityManager();
+		$repository = $em->getRepository('UserBundle:Eleven');
+
+		if($request->getMethod()=='POST')
+		{
+			$username=$request->get('username');
+			$password=$request->get('password');
+			$name=$request->get('name');
+			$email=$request->get('email');
+
+			$user = $repository->findOneBy(array('username'=>$username));
+			if($user)
+			{
+				return $this->render('UserBundle:User:signup.html.twig', array('message' => 'That username is taken. Please choose another one'));
+			}
+			else
+			{
+				$user = new User();
+				$user->setUsername($username);
+				$user->setPassword($password);
+				$user->setName($name);
+				$user->setEmail($email);
+				$user->setPrivileges(false);
+
+				$em = $this->getDoctrine()->getEntityManager();
+				$em->persist($user);
+				$em->flush();
+				return $this->render('UserBundle:User:home.html.twig', array('message' => 'User added to database succesfully'));
+			}
+		}
+		else
+		{
+			return $this->render('UserBundle:User:signup.html.twig', array('message' => 'There was an unexpected problem. Please try again or contact the administrators'));
+		}
+
 		return $this->render('UserBundle:User:index.html.twig');
 	}
 }
