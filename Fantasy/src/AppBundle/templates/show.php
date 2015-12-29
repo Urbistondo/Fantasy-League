8ac8b66b523dcafbe218a79d385adfc58429cd05
Javi Urbistondo <Javi Urbistondo>
2015-12-29 23:02:38 +0100

    Pequeños cambios

--- Fantasy/src/AppBundle/templates/show.php
+++
@@ -1,6 +0,0 @@
-<?php $title = $player['name'] ?>
-<?php $view->extend('::layout.html.php') ?>
-
-	<h1><?php echo $player['name'] ?></h1>
-
-	<div class="name"><?php echo $player['name'] . $player['lastname'] ?></div>
