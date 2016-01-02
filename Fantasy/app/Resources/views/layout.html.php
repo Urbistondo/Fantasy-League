c535976acfbcc3f6c6cce90b3769e1ceb565fa1b
Javi Urbistondo <Javi Urbistondo>
2016-01-02 17:37:36 +0100

    Bootstrap

--- Fantasy/app/Resources/views/layout.html.php
+++
@@ -1,9 +0,0 @@
-<!doctype html>
-<html>
-	<head>
-		<title><?php $view['slots']->output('title', 'Default title') ?></title>
-	</head>
-	<body>
-		<?php $view['slots']->output('_content') ?>
-	</body>
-</html>
