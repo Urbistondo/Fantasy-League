a1773cdb7bcb82b31f2b66a267a08fa5c329eecf
Juan Garayalde <Juan Garayalde>
2016-01-05 21:34:26 +0100

    Pujas

---
+++ Fantasy/src/UserBundle/Controller/MarketController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MarketController extends Controller
+{
+	public function listMarketAction()
+	{
+		$session=$this->getRequest()->getSession();
+		$user=$session->get('user');
+		$league_id=$session->get('league_id');
+		if ($league_id != null)
+		{
+			
+			$market = $this->get('doctrine')->getManager()->getRepository('UserBundle:Market')->findOneBy(array('league_id' => $league_id));
+				if ($market != null)
+				{
+					$aux = array();
+					$aux = $market->getPlayers();
+					$players = array();
+					foreach ($aux as $id)
+					{
+						
+						$player = $this->get('doctrine')->getManager()->getRepository('UserBundle:Player')->findOneBy(array('id' => $id));
+						array_push($players, $player);
+					}
+					return $this->render('UserBundle:User:list.html.twig', array('items' => $players, 'title' => "Market", 'message' => false, 
+						'type' => "Player", 'edit' => 'market'));
+					}
+				
+		}
+		else
+		{
+			return $this->redirectToRoute('user_index');
+		}
+	}
+
+
+
+
+
+}
---
+++ Fantasy/src/UserBundle/Entity/Bid.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Bid
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Bid
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="league_id", type="integer")
+     */
+    private $league_id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="user_id", type="integer")
+     */
+    private $user_id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="player_id", type="integer")
+     */
+    private $player_id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="bid", type="integer")
+     */
+    private $bid;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set leagueId
+     *
+     * @param integer $leagueId
+     *
+     * @return Bid
+     */
+    public function setLeagueId($leagueId)
+    {
+        $this->league_id = $leagueId;
+
+        return $this;
+    }
+
+    /**
+     * Get leagueId
+     *
+     * @return integer
+     */
+    public function getLeagueId()
+    {
+        return $this->league_id;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param integer $userId
+     *
+     * @return Bid
+     */
+    public function setUserId($userId)
+    {
+        $this->user_id = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return integer
+     */
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * Set playerId
+     *
+     * @param integer $playerId
+     *
+     * @return Bid
+     */
+    public function setPlayerId($playerId)
+    {
+        $this->player_id = $playerId;
+
+        return $this;
+    }
+
+    /**
+     * Get playerId
+     *
+     * @return integer
+     */
+    public function getPlayerId()
+    {
+        return $this->player_id;
+    }
+
+    /**
+     * Set bid
+     *
+     * @param integer $bid
+     *
+     * @return Bid
+     */
+    public function setBid($bid)
+    {
+        $this->bid = $bid;
+
+        return $this;
+    }
+
+    /**
+     * Get bid
+     *
+     * @return integer
+     */
+    public function getBid()
+    {
+        return $this->bid;
+    }
+}
+
